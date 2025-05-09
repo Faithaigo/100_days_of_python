43d0487590255dc76d808c28e2d918af0b9c021a
Aigo Faith <aigofaith@gmail.com>
2025-05-09 09:19:57 +0300

    Day 10 code

---
+++ app.py
@@ -0,0 +1,63 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+values_dict = {
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': divide
+}
+
+def calculator():
+    print(logo)
+    is_calculating = True
+
+    n1 = float(input("What's the first number?: "))
+
+    while is_calculating:
+        print('+\n-\n*\n/')
+        operation = input('Pick an operation: ')
+        n2 = float(input("What's the next number?: "))
+        result = values_dict[operation](n1, n2)
+        print(f"{n1} {operation} {n2} = {result}")
+        continue_calc = input(
+            f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ").lower()
+        if continue_calc == 'y':
+            n1 = result
+        else:
+            is_calculating = False
+            print('\n' * 20)
+            calculator()
+
+calculator()
